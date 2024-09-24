ca3a84857f672a7467831a1cfe5157239b1d34ba
Javier Gomez Tagle <100324711+JavierG2003@users.noreply.github.com>
2024-09-24 15:04:56 -0700

    Add files via upload

---
+++ Lab Challenges/3A: Classes (Tortoise vs Hare)/Tortoise.java
@@ -0,0 +1,23 @@
+// implement the Tortoise class here
+public class Tortoise extends Racer {
+    // FIELDS --------------------------------------------------------
+
+    // METHODS -------------------------------------------------------
+    
+    public Tortoise(String name) {
+        super(name, 2.0, 3.0);
+
+    }
+
+    @Override
+    public void run(double timeIntervalInSeconds) {
+        double speed = Math.random() * (this.maxSpeedInMetersPerSecond = this.minSpeedInMetersPerSecond) 
+                                     + minSpeedInMetersPerSecond;
+        
+        double distance = speed * timeIntervalInSeconds;
+
+        this.distanceCoveredInMeters += distance;
+
+        System.out.printf("%s walks %.2f meters; now at marker %.2f\n", this.name, distance, this.distanceCoveredInMeters);
+    }
+}
